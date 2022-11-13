4d2c28705d87e1275c34b40df009703b4f481d81
Ilya Stromov <stromov.ilya@gmail.com>
2022-11-13 18:21:41 +0500

    install debug-toolbar

--- mindb/urls.py
+++ mindb/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +6,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mindb.products.urls", namespace="products")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls')),]
